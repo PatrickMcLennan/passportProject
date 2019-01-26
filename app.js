5fa8261a62cf368dbaf77625095612f9af90e793
Patrick <patrick.a.mclennan@gmail.com>
2019-01-26 12:59:06 -0500

    scaffolding

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
