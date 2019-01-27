819675ec1ae668213e23461c6f484e09c7e950e5
Patrick <patrick.a.mclennan@gmail.com>
2019-01-27 14:19:52 -0500

    workin'

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,83 @@
+// 31:05
+
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+// User Model
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
 
-// Login Page
+// Register Page
 router.get('/register', (req, res) => res.render('register'));
+
+// Register Handle
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all fields' });
+  }
+  // Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: 'Passwords do not Match' });
+  }
+  // Make sure passwords are at least 6 characters long
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    // If there are any errors,
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    // Validation passed, all good
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        // User already exists
+        errors.push({ msg: 'Email has already been registered' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        // User does not exist, Register new one
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+        // Hash users password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            // Set password to hashed
+            newUser.password = hash;
+            // Save user
+            newUser
+              .save()
+              .then(user => res.redirect('/users/login'))
+              .catch(err => console.log(err));
+          })
+        );
+      }
+    });
+  }
+});
 
 module.exports = router;
